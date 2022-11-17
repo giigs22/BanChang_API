90a534cc808b31aa105532103279568209471f7e
GiiGs Thanet <gigslovegod@gmail.com>
2022-11-17 22:58:08 +0700

    Update DataExport#2

--- app/Http/Controllers/DataExportController.php
+++ app/Http/Controllers/DataExportController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\ApiHelper;
+use App\Classes\Helpers;
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class DataExportController extends Controller
 {
+    public function __construct()
+    {
+        $this->api_helper = new ApiHelper;
+        $this->helpers = new Helpers;
+    }
     public function export_csv(Request $request)
     {
         $widget = $request->widget;
@@ -14,8 +22,17 @@ class DataExportController extends Controller
         $option = $request->option;
 
         if($widget == 'env'){
-
+            $device = Device::where('widget_id',1)->get();
         }
+
+        if($freq == 'daily'){
+            $list_data = [];
+            foreach ($device as $key => $value) {
+                $list_data[] = $this->api_helper->getHistoryAPIByDevice($value->device_id,$data,$option['start'],$option['end'],null);
+            }
+            
+        }
+
 
     }
 }
